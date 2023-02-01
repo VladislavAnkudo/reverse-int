46f8bfc25fb4413c5a6eb8cecec78ed96f77a6b8
VladislavAnkudo <ankudo47@gmail.com>
2023-02-01 21:59:04 +0300

    upd: file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let minus = Math.abs(n)
+    let str = String(minus)
+    let number1 = str.split("");
+    let number2 = number1.reverse();
+    let number3 = Number(number2.join(""));
+    return number3
 }
